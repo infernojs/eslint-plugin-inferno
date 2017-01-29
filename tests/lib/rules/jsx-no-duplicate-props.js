1183918d4a8a83e66832ac893de43178dc2aa4bc
Sampo Kivistö <sampo.kivisto@live.fi>
2017-01-29 20:10:06 +0200

    handle namespace attributes, bump 6.9.2

--- tests/lib/rules/jsx-no-duplicate-props.js
+++ tests/lib/rules/jsx-no-duplicate-props.js
@@ -47,7 +47,8 @@ ruleTester.run('jsx-no-duplicate-props', rule, {
     {code: '<App c="a" {...this.props} a="c" b="b" />;', parserOptions: parserOptions},
     {code: '<App A a />;', parserOptions: parserOptions},
     {code: '<App A b a />;', parserOptions: parserOptions},
-    {code: '<App A="a" b="b" B="B" />;', parserOptions: parserOptions}
+    {code: '<App A="a" b="b" B="B" />;', parserOptions: parserOptions},
+    {code: '<App foo:bar={"test"}/>;', options: ignoreCaseArgs, parserOptions: parserOptions}
   ],
   invalid: [
     {code: '<App a a />;', errors: [expectedError], parserOptions: parserOptions},
@@ -55,6 +56,6 @@ ruleTester.run('jsx-no-duplicate-props', rule, {
     {code: '<App a="a" b="b" a="a" />;', errors: [expectedError], parserOptions: parserOptions},
     {code: '<App A a />;', options: ignoreCaseArgs, errors: [expectedError], parserOptions: parserOptions},
     {code: '<App a b c A />;', options: ignoreCaseArgs, errors: [expectedError], parserOptions: parserOptions},
-    {code: '<App A="a" b="b" B="B" />;', options: ignoreCaseArgs, errors: [expectedError], parserOptions: parserOptions}
+    {code: '<App A="a" b="b" B="B" />;', options: ignoreCaseArgs, errors: [expectedError], parserOptions: parserOptions},
   ]
 });
